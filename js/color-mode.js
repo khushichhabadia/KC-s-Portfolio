d91a87f1352ed83a13ab7ee489da72e6a60ed8c8
Khushi Chhabadia <khushichhabadia16@gmail.com>
2024-07-25 16:53:43 +0530

    portfolio

---
+++ js/color-mode.js
@@ -0,0 +1,22 @@
+//Defining DOM elements
+const toggleButton = document.querySelector("#toggle-button");
+const root = document.querySelector(":root");
+const storageKey = "color-mode";
+const deafaultMode = "light-mode";
+
+//Load the user's preffered color mode from local storage
+
+function loadColorMode (){
+
+}
+
+loadColorMode(); //Calling a function
+
+//Toggle between the color mode
+
+toggleButton.addEventListener("click" , ()=>{
+
+});
+
+//Saves user's preffered color mode in local storage
+
